a169d722dca5d1118e0737e21b2dd680f16fa667
Greg Hormann <ghormann@gmail.com>
2024-11-20 19:44:32 +0000

    Improve overlay testing

--- testoverlay.js
+++ testoverlay.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const fpplib = require("./lib/fpp.js");
-const infoboard = new fpplib.FPPApi("192.168.1.148");
+const infoboard = new fpplib.FPPApi("192.168.1.158");
+//const infoboard = new fpplib.FPPApi("192.168.1.148");
 
 
 function sleep(ms) {
@@ -11,11 +12,40 @@ function sleep(ms) {
   }
 
 async function test() {
-    await infoboard.overlaySetText("This is really long");
+    let today = 1242;
+    let season = 10123;
+    let popular = "White";
+    let fontsize = 48;
+    const max_length = 15;
+    const sleep_dur = 2000;
+    let full_message = "Today, there have been " + today + " buttons pressed. " + popular + " has been the most popular today. ";
+    full_message +=  "This season, a total of " + season + " buttons have been pressed!"; 
+
+    let parts = full_message.split(" ");
+    let msg = "";
     await infoboard.overlaySetState(1);
-    await sleep(10000);
+    for (const p of parts) {
+        if ((msg.length + p.length ) < max_length) {
+            msg += " ";
+            msg += p;
+        } else {
+            msg = msg.trim();
+            console.log(msg);
+            await infoboard.overlaySetText(msg, fontsize);
+            await sleep(sleep_dur);
+            msg = p;     
+        }
+    }
+    if (msg.length > 0) {
+        msg = msg.trim();
+        console.log(msg);
+        await infoboard.overlaySetText(msg, fontsize);
+        await sleep(sleep_dur);
+        msg = "";        
+    }
     await infoboard.overlayClearMessage();
     await infoboard.overlaySetState(0);
 }
 
 test();
+setInterval(test, 25000);
