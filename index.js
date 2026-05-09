b68c0301a940f4b4b71e27391248b6f962aba3a1
Greg Hormann <ghormann@gmail.com>
2026-05-09 18:14:37 -0400

    feat: load and persist state cache on startup and shutdown

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const playlist = require("./lib/playlist.js");
 const scheduler = require("./lib/scheduler.js");
 const tunnel = require("./lib/tunnelScheduler.js");
 const mymqtt = require("./lib/mymqtt.js");
+const stateCache = require("./lib/stateCache.js");
 
 model.loadOverrides();
+stateCache.load();
 
 async function init() {
     mymqtt.init();
@@ -18,6 +20,20 @@ async function publishPlaylist() {
     mymqtt.publishPlaylist();
     tunnel.publishButtonMapping();
 }
+
+stateCache.startPeriodicSave();
+
+process.on("SIGTERM", function () {
+    console.log("[stateCache] SIGTERM received, saving state before exit");
+    stateCache.save();
+    process.exit(0);
+});
+
+process.on("SIGINT", function () {
+    console.log("[stateCache] SIGINT received, saving state before exit");
+    stateCache.save();
+    process.exit(0);
+});
 
 setInterval(scheduler.doScheduleCheck, 500);
 setInterval(tunnel.doTunnelCheck, 500);
